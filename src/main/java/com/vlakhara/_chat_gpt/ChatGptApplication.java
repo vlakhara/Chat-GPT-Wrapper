868d288ac84e4a4f73ae21f9e62b67e2b82d6ea1
vlakhara <vipullakhara18@gmail.com>
2024-03-16 00:25:56 +0530

    ✨ Setup

---
+++ src/main/java/com/vlakhara/_chat_gpt/ChatGptApplication.java
@@ -0,0 +1,13 @@
+package com.vlakhara._chat_gpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatGptApplication.class, args);
+	}
+
+}
